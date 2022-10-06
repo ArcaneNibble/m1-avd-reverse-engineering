9ab5af6545dad319db3e1456421d3477e13a6e09
R <rqou@berkeley.edu>
2022-10-06 02:39:30 -0700

    can send command and get reply

--- avd_emu.py
+++ avd_emu.py
@@ -6,6 +6,36 @@ from unicorn.arm_const import *
 
 HACK_WFI_ADDRESS = 0x3f8
 
+def _ascii(s):
+	s2 = ""
+	for c in s:
+		if c < 0x20 or c > 0x7e:
+			s2 += "."
+		else:
+			s2 += chr(c)
+	return s2
+
+def hexdump(s, sep=" "):
+	return sep.join(["%02x"%x for x in s])
+
+def chexdump(s, st=0, abbreviate=True, indent="", print_fn=print):
+	last = None
+	skip = False
+	for i in range(0,len(s),16):
+		val = s[i:i+16]
+		if val == last and abbreviate:
+			if not skip:
+				print_fn(indent+"%08x  *" % (i + st))
+				skip = True
+		else:
+			print_fn(indent+"%08x  %s  %s  |%s|" % (
+				i + st,
+				hexdump(val[:8], ' ').ljust(23),
+				hexdump(val[8:], ' ').ljust(23),
+				_ascii(val).ljust(16)))
+			last = val
+			skip = False
+
 def dump_all_regs(emu_):
 	r0 = emu_.reg_read(UC_ARM_REG_R0)
 	r1 = emu_.reg_read(UC_ARM_REG_R1)
@@ -119,6 +149,26 @@ def read_cm3ctrl_irq_en(addr):
 def read_cm3ctrl_mbox0_retrieve(_addr):
 	return 0x1092ccc
 
+def read_cm3ctrl_irq_status(addr):
+	print(f"NOT IMPLEMENTED: CM3 IRQ status read {addr:08x}")
+
+def write_cm3ctrl_irq_status(addr, val):
+	if addr == 0x5001002c:
+		for i in range(14):
+			if val & (1 << i):
+				print(f"CM3 control clearing IRQ {i}")
+	else:
+		reg_idx = (addr - 0x50010030) // 4
+		for i in range(32):
+			if val & (1 << i):
+				print(f"CM3 control clearing IRQ {14 + reg_idx * 32 + i}")
+
+def write_cm3ctrl_mbox1_submit(addr, val):
+	print(f"mbox uc->ap got something! {val:08x}")
+	real_addr = val + 0xef7_0000
+	reply = emu.mem_read(real_addr, 0x60)
+	chexdump(reply)
+
 MMIOS = {
 	0x50010010: (read_cm3ctrl_irq_en_0, write_cm3ctrl_irq_en_0),
 	0x50010014: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
@@ -127,8 +177,17 @@ MMIOS = {
 	0x50010020: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
 	0x50010024: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
 	0x50010028: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
+	0x5001002c: (read_cm3ctrl_irq_status, write_cm3ctrl_irq_status),
+	0x50010030: (read_cm3ctrl_irq_status, write_cm3ctrl_irq_status),
+	0x50010034: (read_cm3ctrl_irq_status, write_cm3ctrl_irq_status),
+	0x50010038: (read_cm3ctrl_irq_status, write_cm3ctrl_irq_status),
+	0x5001003c: (read_cm3ctrl_irq_status, write_cm3ctrl_irq_status),
+	0x50010040: (read_cm3ctrl_irq_status, write_cm3ctrl_irq_status),
+	0x50010044: (read_cm3ctrl_irq_status, write_cm3ctrl_irq_status),
 
 	0x50010058: (read_cm3ctrl_mbox0_retrieve, lambda _addr, _val: None),
+	0x5001005c: (lambda _addr: 0x20001, lambda _addr, _val: None),	# mbox1 status
+	0x50010060: (lambda _addr: 0xdeadbeef, write_cm3ctrl_mbox1_submit),
 
 
 	0xe000ed08: (lambda _addr: None, write_vtor),
@@ -143,6 +202,9 @@ MMIOS = {
 }
 
 MMIO_BLOCKS = [
+	(0x40100000, 0x8000),	# Config
+	(0x4010c000, 0x4000),	# DMA thingy
+	(0x40400000, 0x4000),	# WrapCtrl
 	(0x50010000, 0x4000),	# CM3Ctrl
 	(0xe000c000, 0x4000),	# SCS
 ]
@@ -155,7 +217,11 @@ emu.mem_map(0x10000000, 0x10000)	# DRAM
 
 emu.mem_write(0, FIRMWARE)
 
-def hook_mmio(emu_, access, addr, sz, value, data):
+def hook_code(emu_, _addr, _sz, _data):
+	# HACK: This doesn't do anything, just disables JIT to get accurate PCs
+	pass
+
+def hook_mmio(emu_, access, addr, sz, value, _data):
 	if addr in MMIOS:
 		if access == UC_MEM_READ:
 			read_fn = MMIOS[addr][0]
@@ -177,6 +243,8 @@ for (addr, len_) in MMIO_BLOCKS:
 	emu.hook_add(UC_HOOK_MEM_READ, hook_mmio, begin=addr, end=addr + len_)
 	emu.hook_add(UC_HOOK_MEM_WRITE, hook_mmio, begin=addr, end=addr + len_)
 
+emu.hook_add(UC_HOOK_CODE, hook_code, begin=0, end=0xffffffff)
+
 ##### kick off!
 
 initial_sp = struct.unpack("<I", FIRMWARE[0:4])[0]
@@ -191,5 +259,15 @@ dump_all_regs(emu)
 save_dram(emu, "avd_ram_after_boot.bin")
 
 # queue a command
-
 trigger_irq(emu, 1)
+
+print("~~~~~ HOPEFULLY PROCESSED COMMAND ~~~~~")
+dump_all_regs(emu)
+save_dram(emu, "avd_ram_after_cmd.bin")
+
+# get the reply
+trigger_irq(emu, 2)
+
+print("~~~~~ HOPEFULLY GOT REPLY ~~~~~")
+dump_all_regs(emu)
+save_dram(emu, "avd_ram_after_reply.bin")
