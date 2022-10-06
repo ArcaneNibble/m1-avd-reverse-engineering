3cb23a9563bc7b78b8d2523da67142c2dc3005e5
R <rqou@berkeley.edu>
2022-10-06 00:23:15 -0700

    irq mmio handling

--- avd_emu.py
+++ avd_emu.py
@@ -2,6 +2,8 @@ import struct
 from unicorn import *
 from unicorn.arm_const import *
 
+##### helpers
+
 def dump_all_regs(emu_):
 	r0 = emu_.reg_read(UC_ARM_REG_R0)
 	r1 = emu_.reg_read(UC_ARM_REG_R1)
@@ -32,29 +34,86 @@ def save_dram(emu_, fn):
 with open('avd-12.3-lilyD-fw.bin', 'rb') as f:
 	FIRMWARE = f.read()
 
-def read_magic_reg():
-	print("READ MAGIC REG")
-	return 0xdeadc0de
+##### MMIO emu logic
 
-def write_magic_reg(val):
-	print(f"WRITE {val:08x} TO MAGIC REG")
+def write_vtor(addr, val):
+	print(f"VTOR = {val:08x}")
 
-def read_magic_reg_2():
-	print("READ MAGIC REG 2")
-	return 0xcafebabe
 
-def write_magic_reg_2(val):
-	print(f"WRITE {val:08x} TO MAGIC REG 2")
+nvic_enabled_irqs = [0] * 8
+
+def write_isen(addr, val):
+	for i in range(32):
+		if val & (1 << i):
+			reg_idx = (addr - 0xe000e100) // 4
+			irq_line = reg_idx * 32 + i
+			print(f"NVIC enabling IRQ {irq_line}")
+			nvic_enabled_irqs[reg_idx] |= (1 << i)
+
+def read_isen(addr):
+	reg_idx = (addr - 0xe000e100) // 4
+	return nvic_enabled_irqs[reg_idx]
+
+
+cm3ctrl_enabled_irq0 = 0
+cm3ctrl_enabled_irqs = [0] * 6
+
+def write_cm3ctrl_irq_en_0(addr, val):
+	global cm3ctrl_enabled_irq0
+	old_val = cm3ctrl_enabled_irq0
+	for i in range(14):
+		if (not (old_val & (1 << i))) and (val & (1 << i)):
+			print(f"CM3 control enabling IRQ {i}")
+		if (old_val & (1 << i)) and (not (val & (1 << i))):
+			print(f"CM3 control disabling IRQ {i}")
+	cm3ctrl_enabled_irq0 = val
+
+def read_cm3ctrl_irq_en_0(_addr):
+	return cm3ctrl_enabled_irq0
+
+def write_cm3ctrl_irq_en(addr, val):
+	global cm3ctrl_enabled_irqs
+	reg_idx = (addr - 0x50010014) // 4
+	old_val = cm3ctrl_enabled_irqs[reg_idx]
+	for i in range(32):
+		if (not (old_val & (1 << i))) and (val & (1 << i)):
+			print(f"CM3 control enabling IRQ {14 + reg_idx * 32 + i}")
+		if (old_val & (1 << i)) and (not (val & (1 << i))):
+			print(f"CM3 control disabling IRQ {14 + reg_idx * 32 + i}")
+	cm3ctrl_enabled_irqs[reg_idx] = val
+
+def read_cm3ctrl_irq_en(addr):
+	reg_idx = (addr - 0x50010014) // 4
+	return cm3ctrl_enabled_irqs[reg_idx]
 
 MMIOS = {
-	0xdeadaa00: (read_magic_reg, write_magic_reg),
-	0xdeadaa04: (read_magic_reg_2, write_magic_reg_2),
+	0x50010010: (read_cm3ctrl_irq_en_0, write_cm3ctrl_irq_en_0),
+	0x50010014: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
+	0x50010018: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
+	0x5001001c: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
+	0x50010020: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
+	0x50010024: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
+	0x50010028: (read_cm3ctrl_irq_en, write_cm3ctrl_irq_en),
+
+	0xe000ed08: (lambda _addr: None, write_vtor),
+	0xe000e100: (read_isen, write_isen),
+	0xe000e104: (read_isen, write_isen),
+	0xe000e108: (read_isen, write_isen),
+	0xe000e10c: (read_isen, write_isen),
+	0xe000e110: (read_isen, write_isen),
+	0xe000e114: (read_isen, write_isen),
+	0xe000e118: (read_isen, write_isen),
+	0xe000e11c: (read_isen, write_isen),
+	# 0xdeadaa00: (read_magic_reg, write_magic_reg),
+	# 0xdeadaa04: (read_magic_reg_2, write_magic_reg_2),
 }
 
 MMIO_BLOCKS = [
 	(0x50010000, 0x4000),	# CM3Ctrl
 	(0xe000c000, 0x4000),	# SCS
 ]
+
+##### setup
 
 emu = Uc(UC_ARCH_ARM, UC_MODE_THUMB | UC_MODE_MCLASS)
 emu.mem_map(0, 0x10000)				# IRAM
@@ -66,11 +125,12 @@ def hook_mmio(emu_, access, addr, sz, value, data):
 	if addr in MMIOS:
 		if access == UC_MEM_READ:
 			read_fn = MMIOS[addr][0]
-			out_val = read_fn()
-			emu_.mem_write(addr, struct.pack("<I", out_val))
+			out_val = read_fn(addr)
+			if out_val is not None:
+				emu_.mem_write(addr, struct.pack("<I", out_val))
 		elif access == UC_MEM_WRITE:
 			write_fn = MMIOS[addr][1]
-			write_fn(value)
+			write_fn(addr, value)
 	else:
 		pc = emu_.reg_read(UC_ARM_REG_PC)
 		if access == UC_MEM_READ:
