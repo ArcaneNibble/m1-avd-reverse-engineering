dc5ca3cafe09d7e83f15fe1f875b7c3bba0df52c
R <rqou@berkeley.edu>
2022-10-05 23:58:18 -0700

    boot firmware to wfi

--- avd_emu.py
+++ avd_emu.py
@@ -2,7 +2,8 @@ import struct
 from unicorn import *
 from unicorn.arm_const import *
 
-code = b"\x4a\xf6\x00\x20\xcd\xf6\xad\x60\x02\xc8\x01\x31\x01\x60"
+with open('avd-12.3-lilyD-fw.bin', 'rb') as f:
+	FIRMWARE = f.read()
 
 def read_magic_reg():
 	print("READ MAGIC REG")
@@ -23,11 +24,16 @@ MMIOS = {
 	0xdeadaa04: (read_magic_reg_2, write_magic_reg_2),
 }
 
+MMIO_BLOCKS = [
+	(0x50010000, 0x4000),	# CM3Ctrl
+	(0xe000c000, 0x4000),	# SCS
+]
+
 emu = Uc(UC_ARCH_ARM, UC_MODE_THUMB | UC_MODE_MCLASS)
 emu.mem_map(0, 0x10000)				# IRAM
 emu.mem_map(0x10000000, 0x10000)	# DRAM
 
-emu.mem_write(0, code)
+emu.mem_write(0, FIRMWARE)
 
 def hook_mmio(emu_, access, addr, sz, value, data):
 	if addr in MMIOS:
@@ -39,16 +45,23 @@ def hook_mmio(emu_, access, addr, sz, value, data):
 			write_fn = MMIOS[addr][1]
 			write_fn(value)
 	else:
+		pc = emu_.reg_read(UC_ARM_REG_PC)
 		if access == UC_MEM_READ:
-			print(f"UNKNOWN read of size {sz} to register {addr:08x}")
+			print(f"UNKNOWN read @ PC {pc:08x} of size {sz} to register {addr:08x}")
 		elif access == UC_MEM_WRITE:
-			print(f"UNKNOWN write of size {sz} to register {addr:08x} with value {value:08x}")
+			print(f"UNKNOWN write @ PC {pc:08x} of size {sz} to register {addr:08x} with value {value:08x}")
 
-emu.mem_map(0xdead8000, 0x4000)
-emu.hook_add(UC_HOOK_MEM_READ, hook_mmio, begin=0xdead8000, end=0xdeadc000)
-emu.hook_add(UC_HOOK_MEM_WRITE, hook_mmio, begin=0xdead8000, end=0xdeadc000)
+for (addr, len_) in MMIO_BLOCKS:
+	emu.mem_map(addr, len_)
+	emu.hook_add(UC_HOOK_MEM_READ, hook_mmio, begin=addr, end=addr + len_)
+	emu.hook_add(UC_HOOK_MEM_WRITE, hook_mmio, begin=addr, end=addr + len_)
+
+initial_sp = struct.unpack("<I", FIRMWARE[0:4])[0]
+initial_pc = struct.unpack("<I", FIRMWARE[4:8])[0]
+print(f"Starting @ {initial_pc:08x} with SP {initial_sp:08x}")
+emu.reg_write(UC_ARM_REG_SP, initial_sp)
+emu.emu_start(initial_pc, 0)
 
 
-emu.emu_start(1, len(code))
+print("~~~~~ HOPEFULLY HIT WFI ~~~~~")
 
-print(hex(emu.reg_read(UC_ARM_REG_R0)))
