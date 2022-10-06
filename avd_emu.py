f79a3aedb9ec05497f0966e6e8e4ffcaf1efd842
R <rqou@berkeley.edu>
2022-10-05 23:41:47 -0700

    better mmio hook

--- avd_emu.py
+++ avd_emu.py
@@ -1,7 +1,7 @@
 from unicorn import *
 from unicorn.arm_const import *
 
-code = b"\x4a\xf6\x00\x20\xcd\xf6\xad\x60\x01\x68\x01\x31\x01\x60"
+code = b"\x4a\xf6\x04\x20\xcd\xf6\xad\x60\x01\x68\x01\x31\x01\x60"
 
 emu = Uc(UC_ARCH_ARM, UC_MODE_THUMB | UC_MODE_MCLASS)
 emu.mem_map(0, 0x10000)				# IRAM
@@ -9,24 +9,22 @@ emu.mem_map(0x10000000, 0x10000)	# DRAM
 
 emu.mem_write(0, code)
 
-def hook_r(emu_, access, addr, sz, value, data):
-	print("HOOK R!")
-	print(access)
-	print(hex(addr))
-	print(hex(sz))
-	print(hex(value))
-	print(data)
-	emu_.mem_write(0xdeadaa00, b'\xaa\xbb')
-def hook_w(emu_, access, addr, sz, value, data):
-	print("HOOK W!")
-	print(access)
-	print(hex(addr))
-	print(hex(sz))
-	print(hex(value))
-	print(data)
+def hook_mmio(emu_, access, addr, sz, value, data):
+	if addr == 0xdeadaa00:
+		if access == UC_MEM_READ:
+			print("READ MAGIC REG")
+			emu_.mem_write(0xdeadaa00, b'\xaa\xbb\xcc\xdd')
+		elif access == UC_MEM_WRITE:
+			print(f"WRITE MAGIC REG {value:08x}")
+	else:
+		if access == UC_MEM_READ:
+			print(f"UNKNOWN read of size {sz} to register {addr:08x}")
+		elif access == UC_MEM_WRITE:
+			print(f"UNKNOWN write of size {sz} to register {addr:08x} with value {value:08x}")
+
 emu.mem_map(0xdead8000, 0x4000)
-emu.hook_add(UC_HOOK_MEM_READ, hook_r, begin=0xdead8000, end=0xdeadc000)
-emu.hook_add(UC_HOOK_MEM_WRITE, hook_w, begin=0xdead8000, end=0xdeadc000)
+emu.hook_add(UC_HOOK_MEM_READ, hook_mmio, begin=0xdead8000, end=0xdeadc000)
+emu.hook_add(UC_HOOK_MEM_WRITE, hook_mmio, begin=0xdead8000, end=0xdeadc000)
 
 
 emu.emu_start(1, len(code))
